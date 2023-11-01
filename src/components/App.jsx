be77a00abb11dd41fceb6a4e13f19359445f30a0
Ridrox93 <popovandrii@yahoo.com>
2023-11-01 19:39:20 +0100

    next

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -26,7 +26,7 @@ export class App extends Component {
     }
   }
 
-  componentDidUpdate(prevState) {
+  componentDidUpdate(prevProps, prevState) {
     if (this.state.contacts !== prevState.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
