71f16642e2e62b64034d7e76f4cf12e129a3e794
Ridrox93 <130097056+Ridrox93@users.noreply.github.com>
2023-11-15 12:22:08 +0100

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,16 +1,16 @@
 import css from './ContactList.module.css';
 
-export const ContactList = props => {
+export const ContactList = ({filteredContacts, handleDeleteContact}) => {
   return (
     <>
       <h2 className={css.name}>Contacts</h2>
       <ul className={css.list}>
-        {props.filteredContacts.map(contact => (
+        {filteredContacts.map(contact => (
           <li className={css.item} key={contact.id}>
             {contact.name} {contact.number}
             <button
               className={css.deleteButton}
-              onClick={() => props.handleDeleteContact(contact.id)}
+              onClick={() => handleDeleteContact(contact.id)}
             >
               Delete
             </button>
